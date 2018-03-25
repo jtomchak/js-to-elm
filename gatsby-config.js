1f79a33c87beb8271ee7b9cbbab80bdf404c4fa0
Jesse Tomchak <jtomchak@gmail.com>
2018-03-25 15:10:45 -0700

    wow, forgot the netlify package

--- gatsby-config.js
+++ gatsby-config.js
@@ -197,6 +197,19 @@ module.exports = {
     "gatsby-plugin-offline",
     "gatsby-plugin-catch-links",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-postcss-sass"
+    "gatsby-plugin-postcss-sass",
+    // make sure to put last in the array
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true // boolean to turn off automatic creation of redirect rules for client only paths
+      }
+    }
   ]
 };
