30e25cb887d4f92a8ec818a073520930dfd5d8f3
Jesse Tomchak <jtomchak@gmail.com>
2018-04-24 22:34:10 -0700

    update with tile and audio on details page

--- src/components/EpisodeTemplateDetails/index.jsx
+++ src/components/EpisodeTemplateDetails/index.jsx
@@ -8,6 +8,7 @@ import Episode from "../Episode/index";
 class EpisodeTemplateDetails extends React.Component {
   render() {
     const { subtitle, author } = this.props.data.site.siteMetadata;
+    // const { title, published, slug, summary, audioUrl } = this.props.data.node;
     const episode = this.props.data.contentfulEpisode;
 
     const homeBlock = (
@@ -24,7 +25,13 @@ class EpisodeTemplateDetails extends React.Component {
         {homeBlock}
         <div className="post-single">
           <div className="post-single__inner">
-            <h1 className="post-single__title">{episode.title}</h1>
+            <h1 className="post-single__title" style={{ paddingBottom: "20px" }}>
+              {episode.title}
+            </h1>
+            <audio className="post__audio" title={episode.title} controls preload="none">
+              <source src={`http://traffic.libsyn.com/jstoelm/${episode.audioUrl}.mp3`} />
+            </audio>
+            <p className="post__description">{episode.summary.internal.content}</p>
             <div className="post-single__body" dangerouslySetInnerHTML={{ __html: episodeHTML }} />
             <div className="post-single__date">
               <em>Published {moment(episode.published).format("D MMM YYYY")}</em>
--- src/templates/episode-template.jsx
+++ src/templates/episode-template.jsx
@@ -37,6 +37,7 @@ export const pageQuery = graphql`
       }
     }
     contentfulEpisode(slug: { eq: $slug }) {
+      audioUrl
       id
       title
       draft
